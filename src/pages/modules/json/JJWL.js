c89ac43299a81f9de6ac67c1aaef211ffc68a19e
EdMasterNava <eduardonava2ms@gmail.com>
2023-04-05 12:18:31 -0700

    Imported Example JSON

---
+++ src/pages/modules/json/JJWL.js
@@ -0,0 +1,67 @@
+export const jjwlJSON = [
+    {
+        "event_id": 0,
+        "event_host": "JJWL",
+        "event_name": "Boston II Adults and Masters Gi",
+        "event_start_date": "2023-04-01 00:00:00",
+        "event_end_date": "2023-04-01 00:00:00",
+        "event_venue": "Boston - MA, DCU Center, Worcester",
+        "event_link": "https://www.jjworldleague.com/events/boston-ii-adultsandmasters-gi"
+    },
+    {
+        "event_id": 5,
+        "event_host": "JJWL",
+        "event_name": "Golden State VII NOGI Adults and Masters",
+        "event_start_date": "2023-04-23 00:00:00",
+        "event_end_date": "2023-04-23 00:00:00",
+        "event_venue": "Stockton, Stockton  Arena",
+        "event_link": "https://www.jjworldleague.com/events/goldenstate-vii-adults-nogi"
+    },
+    {
+        "event_id": 6,
+        "event_host": "JJWL",
+        "event_name": "USA 2023 GI ADULTS AND MASTERS",
+        "event_start_date": "2023-04-29 00:00:00",
+        "event_end_date": "2023-04-29 00:00:00",
+        "event_venue": "Dallas , Comerica Center",
+        "event_link": "https://www.jjworldleague.com/events/usa-2023-gi-adults"
+    },
+    {
+        "event_id": 7,
+        "event_host": "JJWL",
+        "event_name": "USA 2023 NOGI ADULTS AND MASTERS",
+        "event_start_date": "2023-04-29 00:00:00",
+        "event_end_date": "2023-04-29 00:00:00",
+        "event_venue": "Dallas , Comerica Center",
+        "event_link": "https://www.jjworldleague.com/events/usa-2023-nogi-adults"
+    },
+    {
+        "event_id": 12,
+        "event_host": "JJWL",
+        "event_name": "American Open VII GI Adults And Masters",
+        "event_start_date": "2023-05-20 00:00:00",
+        "event_end_date": "2023-05-20 00:00:00",
+        "event_venue": "Orlando , Silver Spur  Arena ",
+        "event_link": "https://www.jjworldleague.com/events/american-open-vii-adults-gi"
+    },
+    {
+        "event_id": 13,
+        "event_host": "JJWL",
+        "event_name": "American Open VII NOGI Adults And Masters",
+        "event_start_date": "2023-05-20 00:00:00",
+        "event_end_date": "2023-05-20 00:00:00",
+        "event_venue": "Orlando , Silver Spur  Arena ",
+        "event_link": "https://www.jjworldleague.com/events/american-open-vii-adults-nogi"
+    },
+    {
+        "event_id": 17,
+        "event_host": "JJWL",
+        "event_name": "New England NOGI Kids, Adults and Masters",
+        "event_start_date": "2023-09-30 00:00:00",
+        "event_end_date": "2023-09-30 00:00:00",
+        "event_venue": "Boston - MA, DCU Center Worcester MA Zip",
+        "event_link": "https://www.jjworldleague.com/events/new-england-adults-nogi"
+    }
+];
+
+export default { jjwlJSON };
