b01ee34e385908be86c8df62ea48617f5eacb52e
EdMasterNava <eduardonava2ms@gmail.com>
2023-04-11 12:53:07 -0700

    Organized Events to hold Search & EventTable

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,17 +1,47 @@
 import * as React from 'react';
 
+import Toolbar from './modules/components/Toolbar';
+
+import Box from '@mui/material/Box';
+
 import Navbar from './modules/views/Navbar';
+import Search from './modules/views/Search';
 import EventTable from './modules/views/EventTable';
 
 import withRoot from './modules/styles/withRoot';
 
+import { matchSorter } from 'match-sorter';
+
+import jjwlJSON from './modules/json/JJWL';
+
 function Events() {
+    const completeEventList = jjwlJSON.jjwlJSON;
+    const searchKey = {keys: ["event_name", "event_venue"]};
+    const [events, setEvents] = React.useState(completeEventList);
+    const [searchInput, setSearchInput] = React.useState('');
+    React.useEffect(() => {
+        if(searchInput === ''){
+            setEvents(completeEventList);
+        }else{
+            setEvents(matchSorter(completeEventList, searchInput, searchKey))
+        }
+    }, [searchInput]);
+    const handleSearch = (search) => {
+        setSearchInput(search);
+    }
     return (
         <>
             <Navbar />
-            {/* Search */}
-            {/* Rendered List */}
-            <EventTable />
+            <Box sx={{py: 2, px: 1, bgcolor: ''}}>
+                <Toolbar />
+                <Box sx={{display: 'flex', flexDirection: 'column',justifyContent: 'center', alignItems: 'center'}}>
+                    <Box>
+                        <Search handleSearch={handleSearch}/>
+                        <EventTable events={events}/>
+                    </Box>
+                </Box>
+            </Box>
+            
         </>
     )
 }
